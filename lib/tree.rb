777a980e736db65deb3e63a2d98a2f2adf28d3cc
Fahad Ahmed <fahad838@gmail.com>
2024-11-17 12:39:50 +0530

    Implement build_tree which builds a BST
    build_tree has three arguments: arr, start,
    end_index.
    
    It is called recursively to build the tree.
    we first get the mid element and create a node
    then pass the arr with new subtree using the mid
    element.

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,32 @@
+# frozen_string_literal: true
+
 require_relative 'node'
 
 class Tree
   attr_accessor :root
 
   def initialize(arr)
-    @root = build_tree(arr.sort)
+    @root = build_tree(arr.sort.uniq)
   end
 
-  def build_tree(arr)
-    
+  def build_tree(arr, start = 0, end_index = arr.length - 1)
+    return nil if start > end_index
+
+    mid = (start + end_index) / 2
+    curr_root = Node.new(arr[mid])
+    curr_root.left = build_tree(arr, start, mid - 1)
+    curr_root.right = build_tree(arr, mid + 1, end_index)
+    curr_root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+test.pretty_print
+
